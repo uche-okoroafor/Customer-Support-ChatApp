d853bd286bc0fd3c0e163acc53548a67ffdd4f27
Uche Okoroafor <ucheokoroafor018@gmail.com>
2022-01-02 22:08:26 +0400

    Update Login.jsx

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,35 +1,30 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
-
-import { useNavigate } from "react-router-dom"; // import Paper from "@mui/material/Paper";
-// import Container from "@mui/material/Container";
-import Paper from "@material-ui/core/Paper";
+import useStyles from "./useStyles";
+import { useNavigate } from "react-router-dom";
+import { Formik, FormikHelpers } from "formik";
+import * as Yup from "yup";
+import { Paper, Typography } from "@mui/material";
 import Box from "@material-ui/core/Box";
-import Container from "@material-ui/core/Container";
 import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { login } from "../../helpers/Apis/login";
+import { SnackBarContext } from "../../contexts/SnackBarContext";
 
 const Login = () => {
-  const { errorMessage, updateLoginContext } = useContext(AuthContext);
-
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  const { updateLoginContext } = useContext(AuthContext);
+  const [isSubmitting, setSubmitting] = useState(false);
   const navigate = useNavigate();
+  const { updateSnackBarMessage } = useContext(SnackBarContext);
+  const classes = useStyles();
 
-  //   const handleChange = e => {
-  //     setState({
-  //         ...fields,
-  //         [e.target.id] : e.target.value
-  //     })
-  // }
-
-  const handleSubmit = () => {
+  const handleSubmit = ({ email, password }) => {
+    setSubmitting(true);
     login({ email, password }).then((data) => {
       if (data.error) {
-        // setSubmitting(false);
-        // updateSnackBarMessage(data.error.message);
+        setSubmitting(false);
+        updateSnackBarMessage(data.error.message);
       } else if (data.success) {
         localStorage.setItem("user-token", data.token);
         updateLoginContext(data.data);
@@ -37,64 +32,154 @@ const Login = () => {
         // should not get here from backend but this catch is for an unknown issue
         console.error({ data });
 
-        // setSubmitting(false);
-        // updateSnackBarMessage("An unexpected error occurred. Please try again");
+        setSubmitting(false);
+        updateSnackBarMessage("An unexpected error occurred. Please try again");
       }
     });
   };
 
   return (
-    <Container>
-      <Typography variant="h3">Login</Typography>
-      <Button onClick={() => navigate("/sign-up")}> create account</Button>
+    <Box className={classes.root}>
+      <Box style={{ position: "relative" }}>
+        <Typography
+          sx={{
+            fontSize: { sm: "1.8rem", xs: "1.5rem" },
+          }}
+          className={classes.welcomeText}
+          align="center"
+        >
+          Welcome to our customer
+          <br /> support room
+        </Typography>
+
+        <Box
+          sx={{
+            textAlign: { xs: "right" },
+            position: { xs: "relative", sm: "absolute" },
+          }}
+          className={classes.createAccountBtn}
+        >
+          {" "}
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={() => navigate("/sign-up")}
+          >
+            <Typography variant="subtitle">Create account</Typography>
+          </Button>
+        </Box>
+      </Box>
 
       <Box display="flex" justifyContent="center">
-        <Paper style={{ width: "70%", marginTop: "3rem", padding: "5rem" }}>
-          <Box>
-            <TextField
-              variant="outlined"
-              id="email"
-              fullWidth
-              margin="normal"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              label="email"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              // helperText={touched.email ? errors.email : ''}
-              // error={touched.email && Boolean(errors.email)}
-              // value={values.email}
-              // onChange={handleChange}
-            />
-          </Box>
-          <Box>
-            <TextField
-              variant="outlined"
-              id="password"
-              label="password"
-              type="password"
-              fullWidth
-              margin="normal"
-              name="password"
-              autoFocus
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-          </Box>
+        <Paper
+          sx={{
+            width: { lg: "30%", md: "40%", sm: "50%", xs: "70%" },
+            marginTop: "2rem",
+            padding: "1rem 2rem",
+            backgroundColor: "rgb(1 ,106, 66 ,0.8)",
+            color: "white",
+          }}
+          className={classes.formContainer}
+        >
+          {" "}
+          <Typography variant="h6" align="center">
+            Sign in
+          </Typography>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            validationSchema={Yup.object().shape({
+              email: Yup.string()
+                .required("Email is required")
+                .email("Email is not valid"),
+              password: Yup.string()
+                .required("Password is required")
+                .max(100, "Password is too long")
+                .min(6, "Password too short"),
+            })}
+            onSubmit={handleSubmit}
+          >
+            {({ handleSubmit, handleChange, values, touched, errors }) => (
+              <form
+                onSubmit={handleSubmit}
+                style={{ padding: "1rem" }}
+                noValidate
+              >
+                <Box>
+                  <TextField
+                    variant="outlined"
+                    id="email"
+                    fullWidth
+                    margin="normal"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    className={classes.textField}
+                    size="small"
+                    label={"Email"}
+                    helperText={touched.email ? errors.email : ""}
+                    error={touched.email && Boolean(errors.email)}
+                    value={values.email}
+                    onChange={handleChange}
+                  />
+                </Box>
+                <Box>
+                  <TextField
+                    variant="outlined"
+                    id="password"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    margin="normal"
+                    size="small"
+                    className={classes.textField}
+                    name="password"
+                    autoComplete="current-password"
+                    helperText={touched.password ? errors.password : ""}
+                    error={touched.password && Boolean(errors.password)}
+                    value={values.password}
+                    onChange={handleChange}
+                  />
+                </Box>
 
-          <Box>
-            <Button variant="contained" onClick={() => handleSubmit()}>
-              Login
-            </Button>
-          </Box>
+                <Box
+                  display="flex"
+                  justifyContent="center"
+                  style={{
+                    paddingTop: "2rem",
+                  }}
+                >
+                  <Button
+                    type="submit"
+                    disabled={isSubmitting}
+                    variant="contained"
+                    style={{ padding: "10px 50px" }}
+                    size="large"
+                    color="primary"
+                  >
+                    {isSubmitting ? (
+                      <CircularProgress
+                        style={{
+                          color: "white",
+                          fontSize: 0,
+                          width: "30px",
+                          height: "30px",
+                        }}
+                      />
+                    ) : (
+                      "Login"
+                    )}
+                  </Button>
+                </Box>
+              </form>
+            )}
+          </Formik>
         </Paper>
       </Box>
-    </Container>
+    </Box>
   );
 };
 
