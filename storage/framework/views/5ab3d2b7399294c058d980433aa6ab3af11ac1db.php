8777ec5824b21a64fdc7f7b19bb5178597c7d486
uche-okoroafor <ucheokoroafo01@gmil.com>
2022-01-15 21:43:02 +0400

    update

---
+++ storage/framework/views/5ab3d2b7399294c058d980433aa6ab3af11ac1db.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ItsolutionStuff.com</title>
+</head>
+
+<body>
+    <h1><?php echo e($details['title']); ?></h1>
+    <p><?php echo e($details['body']); ?></p>
+
+    <p>Thank you</p>
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\customerService\resources\views/emails/myTestMail.blade.php ENDPATH**/ ?>
